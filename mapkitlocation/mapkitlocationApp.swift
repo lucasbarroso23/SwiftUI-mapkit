e9c14a85091092c6a689804bc85516bfe1354d86
barrosol <lucas.barroso@impactzero.pt>
2022-12-14 09:02:57 +0000

    Initial Commit

---
+++ mapkitlocation/mapkitlocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkitlocationApp.swift
+//  mapkitlocation
+//
+//  Created by Lucas Barroso IZ on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct mapkitlocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
